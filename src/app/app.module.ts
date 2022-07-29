e177907aab8344514cfbe539d1a6f83d87ca3561
suralipatel <suralipatel70@gmail.com>
2022-07-29 15:51:01 +0530

    sidenav created home page created employee page created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    HomeComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
